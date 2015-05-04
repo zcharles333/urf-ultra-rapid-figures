1bd5ecf773e95cf75adb064952272d5d159b7a04
sean bennett <seanbennett@college.harvard.edu>
2015-05-04 15:16:33 -0400

    merging

--- project/js/KDA.js
+++ project/js/KDA.js
@@ -3,7 +3,10 @@ KDAVis = function(_parentElement, _data, _metaData){
     this.parentElement = _parentElement;
     this.data = _data;
     this.metaData = _metaData;
-    this.displayData = [];
+    this.kda_selection = "kills"
+    this.brushStart = 0
+    this.brushEnd = 0
+    this.kda_options = {"kills":0, "assists":1, "deaths":2}
 
 
 
@@ -18,11 +21,32 @@ KDAVis = function(_parentElement, _data, _metaData){
 KDAVis.prototype.initVis = function(){
 
     var that = this; // read about the this
-    this.total_days = 398
-    this.period_days = 398
 
     this.width = 650;
     this.height = 500;
+
+    console.log(this.data)
+    var kills = []
+    var assists = []
+    var deaths = []
+    var durations = []
+    keys = Object.keys(this.data).map(function(key) {return parseInt(key)})
+    for (ele in keys) {
+        kills = kills.concat(this.data[ele].unique.kills)
+        assists = assists.concat(this.data[ele].unique.assists)
+        deaths = deaths.concat(this.data[ele].unique.deaths)
+        durations = durations.concat(this.data[ele].unique.duration)
+    }
+    this.defaultData = {}
+    this.defaultData["kills"] = kills
+    this.defaultData["assists"] = assists
+    this.defaultData["deaths"] = deaths
+    this.defaultData["durations"] = durations
+    console.log(this.defaultData)
+
+
+    this.displayData = [this.defaultData];
+    this.defaultName = ["Average"]
 
     // this.descriptions = []
     // for (var i=100;i<116;i++) {
@@ -53,65 +77,14 @@ KDAVis.prototype.initVis = function(){
     this.x_margin = 50
     this.y_margin = 200
 
+    this.x_scale = d3.scale.linear()
+        .range([0+that.x_margin, that.width])
+
+    this.y_scale = d3.scale.linear()
+        .range([that.height-that.y_margin, 0]);
+
     this.g = this.svg.append("g")
     	.attr("transform", "translate(0,0)")
-
-
-    // this.x = d3.scale.linear()
-    //     .range([0+this.x_margin, this.width]);
-
-    // this.y = d3.scale.linear()
-    //     .range([this.height-this.y_margin, 0]);
-
-    // this.x_scale = d3.scale.ordinal()
-    // 	.rangeRoundBands([that.x_margin, this.width],0.1)
-    // 	.domain(that.descriptions)
-
-    // this.y_scale = d3.scale.linear()
-    //     .range([this.height-that.y_margin, 0])
-
-    // this.yAxis = d3.svg.axis()
-    //     .scale(this.y_scale)
-    //     .orient("left");
-
-    // this.xAxis = d3.svg.axis()
-    // 	.scale(this.x_scale)
-    // 	.orient("bottom")
-
-    // this.drawBars = this.g
-    // 	.append("g")
-    // 	.attr("class","rects")
-    // 	.selectAll(".rect")
-    // 	.data(that.descriptions)
-    // 	.enter().append("rect")
-    // 	.attr("class","rect")
-    // 	.style("fill",function(d,i) {return that.metaData.priorities[i]["item-color"]})
-
-    // this.drawAvgs = this.g
-    // 	.append("g")
-    // 	.attr("class","circs")
-    // 	.selectAll(".circ")
-    // 	.data(that.descriptions)
-    // 	.enter().append("circle")
-    // 		.attr("class","circ")
-    // 		.attr("fill","black")
-
-
-    // this.draw_xAxis = this.svg.append("g")
-    // 	.attr("class","x axis")
-    // 	.attr("transform", "translate(0," + (that.height - that.y_margin) +")")
-    // 	.call(this.xAxis)
-
-    // this.draw_xAxis
-    // 	.selectAll(".tick").select("text")
-    // 		.attr("transform", "rotate(-70) translate(-10,-10)")
-    // 		.style("text-anchor","end")
-
-
-    // this.draw_yAxis = this.svg.append("g")
-    //     .attr("class", "y axis")
-    //     .attr("transform", "translate(" + that.x_margin +",0)")
-    //     .call(this.yAxis)
 
     // filter, aggregate, modify data
     this.wrangleData(null);
@@ -122,92 +95,140 @@ KDAVis.prototype.initVis = function(){
 
 KDAVis.prototype.updateVis = function(){
 	var that = this
-	var cys = []
-    this.kda_selection = "kills"
+
     this.x_min = 0
     this.x_max = 0
     this.y_min = 0
     this.y_max = 0
-
-    if (that.displayData.length) {
-        var count_dicts = []
-        this.tuple_arrays = []
-        var keys = []
-        var values = []
-        var sums = []
-        var percentages = []
-        for (var ele in that.displayData) {
-            count_dicts.push(_.countBy(that.displayData[ele].unique[that.kda_selection]))
-            console.log(count_dicts[ele])
-            keys.push(Object.keys(count_dicts[ele]).map(function(key) {return parseInt(key);}))
-            values.push(keys[ele].map(function(key){return count_dicts[ele][key];}))
-            sums.push(d3.sum(values[ele]))
-            percentages.push(values[ele].map(function(value) {return Math.round(value / sums[ele] * 100000)/100000}))
-            var tuple_array = []
-            for (var i in keys[ele]) {
-                tuple_array.push([keys[ele][i],percentages[ele][i]])
+    var displayArray = []
+    //this.brushStart = 600
+    //this.brushEnd = 1900
+    console.log(that.brushStart, that.brushEnd)
+    if (that.brushEnd > that.brushStart) {
+        for (i in that.displayData) {
+            var duration_indices = []
+            for (ele in that.displayData[i]["durations"]) {
+                if (that.displayData[i]["durations"][ele] >= that.brushStart && that.displayData[i]["durations"][ele] <= that.brushEnd) {
+                    duration_indices.push(ele)
+                }
             }
-            that.tuple_arrays.push(tuple_array)
-            this.x_min = d3.min([that.x_min,d3.min(keys[ele])])
-            this.x_max = d3.max([that.x_max,d3.max(keys[ele])])
-            this.y_min = d3.min([that.y_min,d3.min(percentages[ele])])
-            this.y_max = d3.max([that.y_max,d3.max(percentages[ele])])
+            displayArray.push(duration_indices.map(function(d) {
+                return that.displayData[i][that.kda_selection][d]
+            }))
         }
-        console.log(keys)
-        console.log(that.tuple_arrays)
-        this.x_scale = d3.scale.linear()
-            .range([0+that.x_margin, that.width])
-            .domain([that.x_min,that.x_max]);
-
-        this.y_scale = d3.scale.linear()
-            .domain([this.y_min,this.y_max])
-            .range([that.height-that.y_margin, 0]);
-
-         this.lines = d3.svg.line()
-             .x(function(d) {console.log(d); return that.x_scale(d[0])})
-             .y(function(d) {return that.y_scale(d[1])})
-
-        this.kda = this.g.selectAll(".kda")
-        this.kda.remove()
-
-        this.kda = this.g.selectAll(".kda")
-            .data(that.tuple_arrays)
-            .enter()
-            .append("g")
-                .attr("class","kda")
-
-        this.kda.append("path")
-            .attr("class","line")
-            .attr("d",function(d) {return that.lines(d)})
-            .style("stroke", function(d,i){return "blue"})
-            .style("stroke-width", "1px")
-            .style("fill", "none")
     }
-	// for(var i=0; i<this.displayData.length; i++)
-	// {
-	// 	cys.push(that.total_votes[i] / that.total_days * that.period_days)
-	// }
+    else {
+        for (ele in that.displayData) {
+            displayArray.push(that.displayData[ele][that.kda_selection])
+        }
+    }
 
-	// this.y_scale
-	// 	//.domain([0,d3.max(this.displayData)])
-	// 	.domain([0,d3.max([d3.max(this.displayData),d3.max(cys)])])
+    var count_dicts = []
+    this.tuple_arrays = []
+    var keys = []
+    var values = []
+    var sums = []
+    var percentages = []
+    for (var ele in displayArray) {
+        count_dicts.push(_.countBy(displayArray[ele]))
+        console.log(count_dicts[ele])
+        keys.push(Object.keys(count_dicts[ele]).map(function(key) {return parseInt(key);}))
+        values.push(keys[ele].map(function(key){return count_dicts[ele][key];}))
+        sums.push(d3.sum(values[ele]))
+        percentages.push(values[ele].map(function(value) {return Math.round(value / sums[ele] * 100000)/100000}))
+        var tuple_array = []
+        for (var i in keys[ele]) {
+            tuple_array.push([keys[ele][i],percentages[ele][i]])
+        }
+        that.tuple_arrays.push(tuple_array)
+        this.x_min = d3.min([that.x_min,d3.min(keys[ele])])
+        this.x_max = d3.max([that.x_max,d3.max(keys[ele])])
+        this.y_min = d3.min([that.y_min,d3.min(percentages[ele])])
+        this.y_max = d3.max([that.y_max,d3.max(percentages[ele])])
+    }
+    console.log(keys)
+    console.log(that.tuple_arrays)
 
-	// this.draw_yAxis
-	// 	.call(this.yAxis)
+    this.x_scale
+        .domain([that.x_min,that.x_max]);
 
-	// this.drawBars
-	// 	.data(that.displayData)
-	// 	.attr("x",function(d,i){return that.x_scale(that.descriptions[i])})
-	// 	.attr("y",function(d){return that.y_scale(d)})
-	// 	.attr("width", function(d){return that.x_scale.rangeBand()})
-	// 	.attr("height", function(d){return that.height - that.y_scale(d) - that.y_margin})
+    this.y_scale
+        .domain([this.y_min,this.y_max]);
 
-	// this.drawAvgs
-	// 	.data(that.displayData)
-	// 	.attr("cx", function(d,i){return that.x_scale(that.descriptions[i]) + that.x_scale.rangeBand() / 2})
-	// 	//.attr("cy", function(d,i){return that.y_scale(that.total_votes[i] / that.total_days * that.period_days)})
-	// 	.attr("cy", function(d,i){return that.y_scale(cys[i])})
-	// 	.attr("r",7)
+     this.lines = d3.svg.line()
+         .x(function(d) {return that.x_scale(d[0])})
+         .y(function(d) {return that.y_scale(d[1])})
+
+    this.kda = this.g.selectAll(".kda")
+    this.kda.remove()
+
+    this.kda = this.g.selectAll(".kda")
+        .data(that.tuple_arrays)
+        .enter()
+        .append("g")
+            .attr("class","kda")
+
+    this.kda.append("path")
+        .attr("class","line")
+        .attr("d",function(d) {return that.lines(d)})
+        .style("stroke", function(d,i){return "blue"})
+        .style("stroke-width", "1px")
+        .style("fill", "none")
+
+    // if (that.displayData.length) {
+    //     var count_dicts = []
+    //     this.tuple_arrays = []
+    //     var keys = []
+    //     var values = []
+    //     var sums = []
+    //     var percentages = []
+    //     for (var ele in that.displayData) {
+    //         count_dicts.push(_.countBy(that.displayData[ele].unique[that.kda_selection]))
+    //         console.log(count_dicts[ele])
+    //         keys.push(Object.keys(count_dicts[ele]).map(function(key) {return parseInt(key);}))
+    //         values.push(keys[ele].map(function(key){return count_dicts[ele][key];}))
+    //         sums.push(d3.sum(values[ele]))
+    //         percentages.push(values[ele].map(function(value) {return Math.round(value / sums[ele] * 100000)/100000}))
+    //         var tuple_array = []
+    //         for (var i in keys[ele]) {
+    //             tuple_array.push([keys[ele][i],percentages[ele][i]])
+    //         }
+    //         that.tuple_arrays.push(tuple_array)
+    //         this.x_min = d3.min([that.x_min,d3.min(keys[ele])])
+    //         this.x_max = d3.max([that.x_max,d3.max(keys[ele])])
+    //         this.y_min = d3.min([that.y_min,d3.min(percentages[ele])])
+    //         this.y_max = d3.max([that.y_max,d3.max(percentages[ele])])
+    //     }
+    //     console.log(keys)
+    //     console.log(that.tuple_arrays)
+
+    //     this.x_scale
+    //         .domain([that.x_min,that.x_max]);
+
+    //     this.y_scale
+    //         .domain([this.y_min,this.y_max]);
+
+    //      this.lines = d3.svg.line()
+    //          .x(function(d) {console.log(d); return that.x_scale(d[0])})
+    //          .y(function(d) {return that.y_scale(d[1])})
+
+    //     this.kda = this.g.selectAll(".kda")
+    //     this.kda.remove()
+
+    //     this.kda = this.g.selectAll(".kda")
+    //         .data(that.tuple_arrays)
+    //         .enter()
+    //         .append("g")
+    //             .attr("class","kda")
+
+    //     this.kda.append("path")
+    //         .attr("class","line")
+    //         .attr("d",function(d) {return that.lines(d)})
+    //         .style("stroke", function(d,i){return "blue"})
+    //         .style("stroke-width", "1px")
+    //         .style("fill", "none")
+    // }
+
 
 }
 
@@ -220,8 +241,27 @@ KDAVis.prototype.onSelectionChange= function (selected){
  //    else {
  //        this.wrangleData(null);
  //    }
-    this.displayData = selected
+    if (selected.length) {
+        this.displayData = []
+        for (ele in selected) {
+            var placeholder = {}
+            placeholder["kills"] = (selected[ele].unique.kills)
+            placeholder["assists"] = (selected[ele].unique.assists)
+            placeholder["deaths"] = (selected[ele].unique.deaths)
+            placeholder["durations"] = (selected[ele].unique.duration)
+            this.displayData.push(placeholder)
+        }
+    }
+    else {
+        this.displayData = [this.defaultData]
+    }
     console.log(this.displayData)
+    this.updateVis();
+}
+
+KDAVis.prototype.kdaRadioChange= function (selected){
+    console.log(selected)
+    this.kda_selection = selected
     this.updateVis();
 }
 
